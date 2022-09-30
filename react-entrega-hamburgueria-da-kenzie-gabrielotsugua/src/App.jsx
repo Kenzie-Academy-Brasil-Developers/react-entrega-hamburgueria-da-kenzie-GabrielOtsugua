e66c2c3d2677231fee972260ccb29e0a6943dd63
Gabriel <drnkgabriel5@gmail.com>
2022-09-30 13:08:13 -0300

    initial configures

---
+++ react-entrega-hamburgueria-da-kenzie-gabrielotsugua/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { StyleReset } from './components/GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <StyleReset />
+
+
+    </div>
+  );
+}
+
+export default App;
